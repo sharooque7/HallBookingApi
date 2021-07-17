33c5a0ece5ff487a2ec7be7699effaa6ddd78c57
SHAROOQUE <mohammed.sharooque@accenture.com>
2021-07-18 00:17:24 +0530

    first commit

--- HallBooking.js
+++ HallBooking.js
@@ -6,7 +6,7 @@ const uniqid = require("uniqid");
 const app = express();
 app.use(cors());
 app.use(express.json());
-const port = process.env.PORT | 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Running on the port ${port}`));
 
